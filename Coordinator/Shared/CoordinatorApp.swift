61bc0284183bbdf0a47085ab5c847f3749f09703
Timo Müller <timomue11er@icloud.com>
2022-06-14 12:19:23 +0200

    initial commit

---
+++ Coordinator/Shared/CoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorApp.swift
+//  Shared
+//
+//  Created by Timo Müller on 14.06.22.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
